c8b1ae899aafed9bdf78081bfa786e784f18984d
Marnin Audu <msmaudu@gmail.com>
2023-01-10 15:13:30 +0100

    Styled top of the program section

--- src/pages/exercises/exercises.jsx
+++ src/pages/exercises/exercises.jsx
@@ -4,19 +4,13 @@ import styles from "./exercises.module.css";
 
 export const Exercises = () => {
   return (
-    <div className={styles.exercise}>
-      <div className={styles.header}>
-        <span className={styles.header__text}>Popular Exercises</span>
-        <a className={styles.more}>SEE MORE EXERCISES</a>
-      </div>
-      <div className={styles.exercises}>
-        <Card name="Treadmill" key="1" />
-        <Card name="Stretching" key="2" />
-        <Card name="Yoga" key="3" />
-        <Card name="Running" key="4" />
-        <Card name="Lifting" key="5" />
-        <Card name="Pushing" key="6" />
-      </div>
+    <div className={styles.exercises}>
+      <Card name="Treadmill" key="1" />
+      <Card name="Stretching" key="2" />
+      <Card name="Yoga" key="3" />
+      <Card name="Running" key="4" />
+      <Card name="Lifting" key="5" />
+      <Card name="Pushing" key="6" />
     </div>
   );
 };
--- src/pages/program/program.jsx
+++ src/pages/program/program.jsx
@@ -1,5 +1,23 @@
 import React from "react";
-
+import styles from "./program.module.css";
 export const Program = () => {
-  return <div>program</div>;
+  return (
+    <section className={styles.program}>
+      <div className={styles.program__top}>
+        <div className={styles.top__left}>Workout Program Made For You</div>
+        <div className={styles.top__right}>
+          <div className={styles.top__right__content}>
+            <span className={styles.top__right__text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+              consectetur adipiscing.
+            </span>
+
+            <button className={styles.btn__area}>Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.program__bottom}></div>
+    </section>
+  );
 };
